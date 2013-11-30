6f666dfbfcba443e815298cd0fd770c127bbe8c2
Ken Lu <ken@seeice.com>
2013-11-30 10:27:06 -0500

    init showcase project for mobile hackathon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showcase::Application.config.secret_key_base = 'e422d58111b6d2112bd739cfcddc50a67b4d1c69bfea301ad1dc64cf2ee22ccd3a4962045eced2db62d170fda8486c50e73f8a9a217d7e011acdbc7114f4a308'
